63d3ea19054d5cfbab9d65ae2692fd4cd49a90eb
surekha-degala <surekhadegala22@gmail.com>
2025-06-28 14:44:40 +0530

    Create pattern2.java
    problem statement
    Sam is making a forest visualizer. An N-dimensional forest is represented by the pattern of size NxN filled with ‘*’.
    
    An N/2-dimensional forest is represented by the lower triangle of the pattern filled with ‘*’.
    
    For every value of ‘N’, help sam to print the corresponding N/2-dimensional forest.
    
    Example:
    Input:  ‘N’ = 3
    
    Output: 
    * 
    * *
    * * *

---
+++ basic-problems/pattern2.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class Solution {
+    public static void nForest(int n) {
+        // Write your code here
+        for ( int i =0;i<n;i++) {
+            for (int j =0;j<=i;j++) {
+                System.out.print("* ");
+                
+            }
+            System.out.println();
+        }
+
+    }
+
+    public static void main (String [] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        nForest(n);
+    }
+}
