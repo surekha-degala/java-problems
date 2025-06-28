453c82b46e9fe9484f2cfc3b2e555de68e1f0a0d
surekha-degala <surekhadegala22@gmail.com>
2025-06-28 22:00:20 +0530

    Create reverse a string.java
    You are given a string s, and your task is to reverse the string.

---
+++ basic-problems/reverse a string.java
@@ -0,0 +1,9 @@
+// User function Template for Java
+class Solution {
+    public static String reverseString(String s) {
+      StringBuilder sb = new StringBuilder(s);
+sb.reverse();
+return sb.toString();
+
+    }
+}
