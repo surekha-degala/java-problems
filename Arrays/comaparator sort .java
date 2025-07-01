c211c62c4736bd993255513d9f8798f6f8f56e04
surekha-degala <surekhadegala22@gmail.com>
2025-07-01 17:31:52 +0530

    Create comaparator sort .java
    Comparator Sort Strings
    
    You are given an string array arr[]. You need to sort the array lexicographically while ignoring the case
    
    Examples:
    
    Input: arr[] = ["Abc", "Xyz", "fghfg", "adas", "tuTY", "dsa"]
    Output: Abc adas dsa fghfg tuTY Xyz
    Explanation: We ignore the case and sort it lexicographically, so we get Abc adas dsa fghfg tuTY Xyz.

---
+++ Arrays/comaparator sort .java
@@ -0,0 +1,9 @@
+// User function Template for Java
+class Solution {
+    public static void sortByComparator(String[] arr) {
+        // Your code here
+        Arrays.sort(arr, String.CASE_INSENSITIVE_ORDER);
+
+        // You sort it in a single line using method references
+    }
+}
