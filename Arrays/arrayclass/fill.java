87f53a920995aeaa7f7d5536fc88b3f2281c5121
surekha-degala <surekhadegala22@gmail.com>
2025-07-01 18:01:03 +0530

    Create fill.java
    Fill Method
    
    You are given an integer array arr[], you are also given a value k. You need to fill arr with k.
    
    Examples:
    
    Input: arr[] = [1, 2, 3, 4, 5], k = 67
    Output: 67 67 67 67 67
    Explanation: k is 67, so we fill arr with 67.

---
+++ Arrays/arrayclass/fill.java
@@ -0,0 +1,8 @@
+// User function Template for Java
+class Solution {
+    public static void fillArray(int arr[], int k) {
+        // Your code here
+        Arrays.fill(arr, k);
+          // You don't need to print
+    }
+}
