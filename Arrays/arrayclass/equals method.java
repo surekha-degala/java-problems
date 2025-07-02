1185c20ed34aac11437b0c80fb631a3657802d0e
surekha-degala <surekhadegala22@gmail.com>
2025-07-02 11:41:08 +0530

    Create equals method.java
    Equals Method
    
    Given a two integer arrays a[] and b[], you need to find if both the arrays contain the same elements in the same order or not.
    
    Example 1:
    
    Input: a[] = [1, 2, 3, 4], b[] = [4, 3, 2, 1]
    Output: false
    Explanation: Both arrays are not same as they don't contain elements in the same order. So we print false.

---
+++ Arrays/arrayclass/equals method.java
@@ -0,0 +1,13 @@
+// User function Template for Java
+class Solution {
+    public static boolean areEqual(int a[], int b[]) {
+        // Your code here
+        if (Arrays.equals(a, b)) {
+            // System.out.println("false");
+            return true;
+        } else {
+          
+          return false;
+        }
+    }
+}
