932beeccb59761fa6ccb54de7c817a9f3aee2887
surekha-degala <surekhadegala22@gmail.com>
2025-07-02 14:37:27 +0530

    Create pyramid p6.java
       *
     ***
    *****

---
+++ basic-problems/patterns/pyramid p6.java
@@ -0,0 +1,19 @@
+public class Solution {
+    public static void nStarTriangle(int n) {
+        // Write your code here
+        for (int i =0; i<n;i++) {
+            for (int j =0; j<n-i-1; j++) {
+                System.out.print(" ");
+            }
+                for ( int k =0; k<2*i+1; k++) {
+                System.out.print("*");
+                }
+                for (int l =0; l <n-i-1; l++) {
+                    System.out.print(" ");
+                }
+            
+        
+        System.out.println();
+    }
+}
+}
