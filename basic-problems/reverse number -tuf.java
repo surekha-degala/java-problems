1e1265f1f3fea8f80c59bf79452ccbada54e47f4
surekha-degala <surekhadegala22@gmail.com>
2025-07-03 17:20:28 +0530

    Update reverse number -tuf.java

--- basic-problems/reverse number -tuf.java
+++ basic-problems/reverse number -tuf.java
@@ -6,3 +6,16 @@ class Solution {
         return Integer.parseInt(reverse);
     }
 }
+
+class Solution {
+  public int reverseNumber(int n) {
+    int rev =0;
+    while (n>0) {
+    
+        int ld = n%10;
+        rev = (rev * 10) +ld;
+        n = n/10;
+    }
+return rev;
+  }
+}
