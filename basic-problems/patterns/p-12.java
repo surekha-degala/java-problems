9499c55b79a06099a2c8a1985061361f77db1ef9
surekha-degala <surekhadegala22@gmail.com>
2025-07-03 10:56:53 +0530

    Create p-12.java
    Problem statement
    
    Send feedback
    Aryan and his friends are very fond of patterns. For a given integer ‘N’, they want to make the Increasing Letter Triangle.
    
    Example:
    Input: ‘N’ = 3
    
    Output: 
    
    A
    A B
    A B C

---
+++ basic-problems/patterns/p-12.java
@@ -0,0 +1,11 @@
+public class Solution {
+    public static void nLetterTriangle(int n) {
+        // Write your code here
+        for (int i = 0; i<n;i++) {
+            for (char ch ='A'; ch<='A'+i; ch++) {
+                System.out.print(ch+" ");
+            }
+            System.out.println();
+        }
+    }
+}
