38878ee80a9145e893776bfbfb6dd09b19d12a0b
surekha-degala <surekhadegala22@gmail.com>
2025-07-16 23:42:09 +0530

    Update Second Largest.java

--- Arrays/Second Largest.java
+++ Arrays/Second Largest.java
@@ -17,3 +17,24 @@ class Solution {
     return secondLargest;
 }
 }
+
+class Solution {
+    public static int secondLargest(int[] arr) {
+        int n = arr.length;
+        if (n < 2) return -1;
+
+        int first = Integer.MIN_VALUE;
+        int second = Integer.MIN_VALUE;
+
+        for (int num : arr) {
+            if (num > first) {
+                second = first;
+                first = num;
+            } else if (num > second && num != first) {
+                second = num;
+            }
+        }
+
+        return (second == Integer.MIN_VALUE) ? -1 : second;
+    }
+}
