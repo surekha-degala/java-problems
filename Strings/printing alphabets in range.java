2272aeb47598f3a2dd06ce809df56bcbb0fde555
surekha-degala <surekhadegala22@gmail.com>
2025-08-04 17:43:24 +0530

    Create printing alphabets in range.java

---
+++ Strings/printing alphabets in range.java
@@ -0,0 +1,15 @@
+// User function Template for Java
+
+// function should print the alphabets
+// from c1 to c2 (space separated)
+// No need to print the new line
+// The new line will be printed by the
+// driver's code.
+class Solution {
+    public static void alphabets(char c1, char c2) {
+        // code here
+        for (char c = c1; c<= c2; c++) {
+            System.out.print(c +" ");
+        }
+    }
+}
