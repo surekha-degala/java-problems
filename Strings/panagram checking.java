695417a29c6279c597253d06e2e491ef1692d356
surekha-degala <surekhadegala22@gmail.com>
2025-08-05 11:55:02 +0530

    Update panagram checking.java

--- Strings/panagram checking.java
+++ Strings/panagram checking.java
@@ -35,3 +35,26 @@ class Solution {
         return set.size() == 26 ? true : false;
     }
 }
+//frequency array
+
+// User function Template for Java
+
+class Solution {
+
+    // Complete the function
+    // str: input string
+    public static boolean isPanagram(String str) {
+        // Check if given string is panagram
+        str = str.toLowerCase();
+        boolean [] present = new boolean [26];
+        for(char ch : str.toCharArray()) {
+            if (Character.isLetter(ch) ){
+                present[ch - 'a'] = true;
+            }
+        }
+        for (boolean b : present) {
+            if (!b) return false;
+        }
+        return true;    
+    }
+}
