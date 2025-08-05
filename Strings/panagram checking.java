bf4a34cc2df86d6d18037dc255a9f52871011675
surekha-degala <surekhadegala22@gmail.com>
2025-08-05 11:38:18 +0530

    Update panagram checking.java

--- Strings/panagram checking.java
+++ Strings/panagram checking.java
@@ -15,3 +15,23 @@ class Solution {
             return true;
         }
     }
+//using hash set
+
+// User function Template for Java
+
+class Solution {
+
+    // Complete the function
+    // str: input string
+    public static boolean isPanagram(String str) {
+        // Check if given string is panagram
+        str = str.toLowerCase();
+        Set<Character> set = new HashSet<>();
+        for(char ch : str.toCharArray()) {
+            if (Character.isLetter(ch) ){
+                set.add(ch);
+            }
+        }
+        return set.size() == 26 ? true : false;
+    }
+}
